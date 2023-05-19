26c043ec296fb45b878b6d4a7a071b19b6d1891a
Nathanuel Dixon <nathan0322@hotmail.com>
2023-05-19 18:13:52 -0400

    DONE, just needa cleanup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
-import { useEffect, useState, useRef } from 'react';
-import { useGeolocated } from 'react-geolocated';
-import {FaMapMarkerAlt} from 'react-icons/fa'
-import Map, { Marker } from 'react-map-gl';
+import { useEffect, useState, useRef, useMemo } from 'react';
+import { FaTruck } from 'react-icons/fa'
+import Map, { Marker, Source, Layer } from 'react-map-gl';
 import ReactGoogleAutocomplete from 'react-google-autocomplete';
+import ReactSwitch from 'react-switch';
+import { BsSun, BsMoon } from 'react-icons/bs';
+import { useTransition, animated } from 'react-spring';
 
 function App() {
   const [places, setPlaces] = useState([])
   const [filteredplaces, setFilteredplaces] = useState([])
   const [mapstats, setMapstats] = useState({})
   const [mapReady, setMapReady] = useState(false)
-  const [isEnterPressed, setIsEnterPressed] = useState(false);
+  const [checked, setChecked] = useState(false);
   const mapRef = useRef();
   
   const onMapLoad = ( map, event ) => {
@@ -23,7 +25,13 @@ function App() {
   const onMarkerClick = (markerid, lat, lng) => {
     console.log('This is ->', markerid)
     // inside the map instance you can call any google maps method
-    mapRef.current.setCenter({ lat, lng })
+    // mapRef.current.setCenter({ lat, lng })
+    mapRef.current.easeTo({
+      center: [lng, lat],
+      duration: 2500,
+      zoom: 14.5
+    });
+    setCurrentPlace(markerid)
     // mapRef.current.panTo(new window.google.maps.LatLng(lat, lng))
   }
 
@@ -31,110 +39,168 @@ function App() {
     fetch("https://data.sfgov.org/resource/rqzj-sfat.json")
       .then(response => response.json())
       .then(data => {
-        console.log("data is: ", data)
-        console.log("type of data is: ", typeof data)
-        setPlaces(data)
+        // const geojson = {
+        //   type: 'FeatureCollection',
+        //   features: []
+        // };
+        
+        // console.log("data is: ", data)
+        // console.log("type of data is: ", typeof data)
+        setPlaces(data.filter((place) => parseFloat(place.latitude) > 0)) 
+        // data.forEach((place) => {
+        //   if (parseFloat(place.latitude) > 0) {
+        //     geojson.features.push({
+        //       type: 'Feature',
+        //       geometry: {
+        //         type: 'Point',
+        //         coordinates: [parseFloat(place.longitude), parseFloat(place.latitude)]
+        //       },
+        //       properties: {
+        //         title: place.applicant,
+        //         description: place.optionaltext,
+        //         facilitytype: place.facilitytype,
+        //         address: place.address,
+        //         fooditems: place.fooditems ? place.fooditems : ''
+        //       }
+        //     })
+        //   }
+        // })
+        // console.log("geojson is: ", geojson)
+        // setPlaces(geojson)
+
+        // mapRef.current.loadImage('https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png', (error, image) => {
+        //     if (error) throw error;
+        //     console.log("image is: ", image)
+        //     mapRef.current.addImage('custom-marker', image);
+        //     // Add a GeoJSON source with 2 points
+        //     mapRef.current.addSource('points', {
+        //       type: 'geojson',
+        //       data: geojson
+        //       // geojson: places
+        //     });
+        //   });
+        // })
       })
-    console.log("coords are: ", coords)
   }, [])
 
-  const { coords, isGeolocationAvailable, isGeolocationEnabled } = useGeolocated({
-      positionOptions: {
-        enableHighAccuracy: true,
-      },
-      userDecisionTimeout: Infinity,
-    });
+  const markers = useMemo(() => filteredplaces.map((place, index) => (
+    <Marker key={index} latitude={parseFloat(place.latitude)} longitude={parseFloat(place.longitude)}>
+      <FaTruck size={20} color={checked ? "gold" : 'red'} onClick={() => onMarkerClick(place.objectid, parseFloat(place.latitude), parseFloat(place.longitude))} />
+    </Marker>
+  )), [checked, filteredplaces]);
 
-  const [viewport, setViewport] = useState({
-    // latitude: coords ? coords.latitude : 38.8951,
-    latitude: 37.773972,
-    // longitude: coords ? coords.longitude : -77.0364,
-    longitude: -122.431297,
-    zoom: 11
-  });
-
-  // const handleKeyDown = (e) => {
-  //   if (e.key === 'Enter') {
-  //     setIsEnterPressed(true);
-  //   } else {
-  //     setIsEnterPressed(false);
-  //   }
-  // };
+  const [viewport, setViewport] = useState({ latitude: 37.773972, longitude: -122.431297, zoom: 11 });
 
   useEffect(() => {
     // whenever map is changed, check to see if the latitude and longitude of each place is within the bounds of the map, display if it is
     if (mapReady) {
       const placesWithinBounds = places.filter(place => {
-        // console.log("parse latitude is: ", parseFloat(place.latitude), "parse longitude is: ", parseFloat(place.longitude))
-        // if (parseFloat(place.latitude) > 90
         return mapstats.bounds.contains([parseFloat(place.longitude), parseFloat(place.latitude)])
       })
-      console.log("places within bounds are: ", placesWithinBounds)
+      // console.log("places within bounds are: ", placesWithinBounds)
       setFilteredplaces(placesWithinBounds)
     }
   }, [mapReady, mapstats, places])
+  
+  const [currentplace, setCurrentPlace] = useState(null);
+
+  const transitions = useTransition(currentplace, {
+    from: { opacity: 0, transform: 'translate3d(50%,-50%,0)'},
+    enter: { opacity: 1, transform: 'translate3d(-50%,-50%,0)' },
+    leave: { opacity: 0, transform: 'translate3d(-150%,-50%,0)', marginRight: '2rem' },
+    // config: { duration: 30000 },
+  })
 
   return (
-    <div className="App">
-      {!isGeolocationAvailable ? (
-        <div>Your browser does not support Geolocation</div>
-      ) : !isGeolocationEnabled ? (
-        <div>Checking to see if your geolocation is enabled...</div>
-      ) : coords ? (
-        <div style={{ height: '100vh', width: '100%' }}>
-          {/* <input id='searchbar' value={searchinput} onChange={(e) => setSearchinput(e.target.value)} placeholder='Enter Location' 
-            style={{zIndex: 999}}
-          /> */}
-          <ReactGoogleAutocomplete
-            apiKey={process.env.REACT_APP_API_KEY}
-            onPlaceSelected={(place) => {
-              console.log("place pressed is:", place);
-              if (place) {
-                if (place.geometry) {
-                  mapRef.current.easeTo({
-                    center: [place.geometry.location.lng(), place.geometry.location.lat()],
-                    duration: 2500
-                  });
-                } else {
-                  alert("Please select a valid location!")
-                }
-                setIsEnterPressed(false);
+    <div className="App" style={{ height: '100vh', width: '100%' }}>
+      <div className='title absolute my-0 z-10 font-bold'>
+        <p>San Fran Food Trucks</p>
+        <ReactGoogleAutocomplete
+          apiKey={process.env.REACT_APP_API_KEY}
+          options={{types: ['address']}}
+          onPlaceSelected={(place) => {
+            console.log("place pressed is:", place);
+            if (place) {
+              if (place.geometry) {
+                mapRef.current.easeTo({
+                  center: [place.geometry.location.lng(), place.geometry.location.lat()],
+                  duration: 1000, zoom: 15
+                });
+              } else {
+                alert("Please select a valid location!")
               }
+            }
+          }}
+          id='searchbar'
+        />
+      </div>
+      <div id="switchparent" className='absolute top-6 z-50'>
+        <ReactSwitch
+          checked={checked}
+          onChange={() => setChecked(!checked)}
+          handleDiameter={28}
+          offColor="#20232a"
+          onColor="#20232a"
+          offHandleColor="#5F9EA0"
+          onHandleColor="#5F9EA0"
+          height={40}
+          width={70}
+          borderRadius={6}
+          activeBoxShadow="0px 0px 1px 2px #fffc35"
+          uncheckedIcon={<div className="flex flex-col justify-center items-center"><BsSun size={20} color="white" /></div>}
+          checkedIcon={<div className="flex flex-col justify-center items-center"><BsMoon size={20} color="white" /></div>}
+          // uncheckedHandleIcon={<BsSun size={20} color="orange" />}
+          // checkedHandleIcon={<BsMoon size={20} color="yellow" />}
+          className="react-switch"
+          id="small-radius-switch"
+        />
+      </div>
+      <Map
+        ref={mapRef}
+        id='mainmap'
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        onViewportChange={setViewport}
+        onLoad={onMapLoad}
+        onMoveEnd={() => {
+          console.log("new zoom is: ", mapRef.current.getZoom())
+          setMapstats({bounds: mapRef.current.getBounds(), zoom: mapRef.current.getZoom()})
+        }}
+        initialViewState={viewport}
+        style={{ width: '100vw', height: '100vh' }}
+        mapStyle={checked ? "mapbox://styles/mapbox/navigation-night-v1" : "mapbox://styles/mapbox/streets-v12"}
+      >
+        {/* <Source type="geojson" data={places}>
+          <Layer
+            id="point"
+            type="symbol"
+            // paint={{
+            //   'circle-radius': 6,
+            //   'circle-color': '#B42222'
+            // }}
+            layout={{
+              'icon-image': 'star',
+              'text-field': ['get', 'title'],
+              'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+              'text-offset': [0, 1.25],
+              'text-anchor': 'top'
             }}
-            id='searchbar'
           />
-          <Map
-            ref={mapRef}
-            id='mainmap'
-            mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-            onViewportChange={setViewport}
-            onLoad={onMapLoad}
-            onMoveEnd={() => {
-              console.log("new zoom is: ", mapRef.current.getZoom())
-              setMapstats({bounds: mapRef.current.getBounds(), zoom: mapRef.current.getZoom()})
-            }}
-            initialViewState={viewport}
-            style={{ width: '100vw', height: '100vh' }}
-            mapStyle="mapbox://styles/mapbox/streets-v9"
-          >
-            {filteredplaces.map((place, index) => {
-              return (
-                <Marker key={index} anchor="bottom" latitude={parseFloat(place.latitude)} longitude={parseFloat(place.longitude)}
-                  style={{transform: 'none !important'}}
-                  // pitchAlignment='viewport' 
-                  // offset={[0, 180]}
-                  onClick={() => onMarkerClick(index, parseFloat(place.latitude), parseFloat(place.longitude))}
-                >
-                <FaMapMarkerAlt className='marker-icon' size={40} />
-                {/* {mapstats.zoom >= 12 && <p className='applicanttext'>{place.applicant}</p>} */}
-              </Marker>
-              )
-            })}
-          </Map>
-        </div> 
-      ) : (
-        <div>Getting the location data&hellip; </div>
-      )}
+        </Source> */}
+        {markers}
+      </Map>
+      {filteredplaces.length > 0 && transitions((style, objectId) => {
+        const filteredPlace = filteredplaces.find(place => place.objectid === objectId);
+        if (!filteredPlace) return null;
+        return (
+          <animated.div id='transitiondiv' style={style} className='flex flex-col absolute left-1/2'>
+            {/* <div id="locationdiv" className='absolute'> */}
+            <p>{filteredPlace.applicant}</p>
+            <p>{filteredPlace.address}</p>
+            <p>{filteredPlace.fooditems && filteredPlace.fooditems.charAt(0).toUpperCase() + filteredPlace.fooditems.slice(1)}</p>
+            {/* </div> */}
+          </animated.div>
+        );
+      })}
     </div>
   );
 }
